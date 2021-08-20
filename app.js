08c19a66b630a68004a5b4c26319129d32bbebe4
Foysal <foysalarif001@gmail.com>
2021-08-21 03:09:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let memoryPrice = document.getElementById('memory-button').addEventListener('click',
+    function() {
+        const memory = document.getElementById('memory-cost');
+        const memoryCost = memory.value;
+        memory = memoryCost;
+    });
